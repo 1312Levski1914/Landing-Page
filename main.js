9fffb74d244c1c64c2c2d4ea3fa6179412197202
Asen K <krushkov1914@gmail.comgit$git config --global user.email krushkov1914@gmail.com>
2022-04-21 07:52:12 +0200

    last updates
    btn and credit text

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ let videoBackground = document.getElementById('myVideo');
 let btnVideo = document.getElementById('btnVideo');
 let videoContainer = document.getElementById('musicVideo');
 let video = document.getElementById('video');
+let h1Tag = document.getElementsByClassName('h1');
+
 
 btnVideo.addEventListener('click',function(){
     if(videoContainer.style.display !== 'block'){
         videoContainer.style.display = 'block';
         video.play();
+        btnVideo.style.display = 'none';
+        h1Tag[0].style.top = '-300px';
+        videoBackground.style.opacity = '0.5'
+
     }else{
         videoContainer.style.display = 'none';
         video.pause();
